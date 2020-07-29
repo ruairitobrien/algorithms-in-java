4be8a43394e4baac5f5a88cb4fe5fab12d76c904
Ruairi O Brien <ruairitobrien@gmail.com>
2020-07-29 15:12:22 +0100

    Update UnionFindTest.java

--- src/test/java/algoritms/unionfind/UnionFindTest.java
+++ src/test/java/algoritms/unionfind/UnionFindTest.java
@@ -2,7 +2,7 @@ package algoritms.unionfind;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;;
+import org.junit.jupiter.api.Test;
 
 public class UnionFindTest {
 
